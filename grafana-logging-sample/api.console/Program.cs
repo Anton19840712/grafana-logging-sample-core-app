91319a284d64bbc5b533bf17e0c912b27c1b73ac
Anton Gridushko <gridushko.anton@yahoo.com>
2024-01-25 18:54:28 +0300

    Add sample configs.

--- grafana-logging-sample/api.console/Program.cs
+++ grafana-logging-sample/api.console/Program.cs
@@ -1,8 +1,21 @@
 ﻿using api.console;
+using LokiLoggingProvider.Options;
 using Microsoft.Extensions.Logging;
 
-//using var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
-using var loggerFactory = LoggerFactory.Create(builder => builder.AddJsonConsole());
+using var loggerFactory = LoggerFactory.Create(
+	builder => builder
+		.AddConsole()
+		.AddLoki(loggerOptions =>
+		{
+			loggerOptions.Client = PushClient.Grpc;
+			//loggerOptions.Formatter = Formatter.Json;
+
+			//additional filters for log search:
+			loggerOptions.StaticLabels.JobName = "loki-console-app";
+			loggerOptions.StaticLabels.AdditionalStaticLabels.Add("system-name", "loki-using");
+		}));
+
+//using var loggerFactory = LoggerFactory.Create(builder => builder.AddJsonConsole());
 var logger = loggerFactory.CreateLogger<Program>();
 
 logger.LogInformation("testing info logging process...");
--- grafana-logging-sample/api.weather/Program.cs
+++ grafana-logging-sample/api.weather/Program.cs
@@ -1,3 +1,6 @@
+using LokiLoggingProvider.Options;
+using Microsoft.Extensions.Logging.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Logging.AddLoki(loggerOptions =>
+{
+	loggerOptions.Client = PushClient.Grpc;
+	loggerOptions.Formatter = Formatter.Json;
+
+	//additional filters for log search:
+	loggerOptions.StaticLabels.JobName = "loki-web-app";
+	loggerOptions.StaticLabels.AdditionalStaticLabels.Add("system-name", "loki-using");
+});
 
 var app = builder.Build();
 
@@ -21,3 +33,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
